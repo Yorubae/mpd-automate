09edec0b1b101a412c12258302297a453fc7d433
Yoru <rickygogoi500@gmail.com>
2023-07-01 18:45:09 +0530

    initial test

---
+++ config.py
@@ -0,0 +1,6 @@
+Path = "/home/Yoru/Music"
+TempPath = "/home/Yoru/yoru-dump/auto-metaeditor/tracks"
+clientId = "63ea96f0776b4fa2b39df21a576ce927"
+clientSecret = "142f38ff0c6541f19e311d7b5670cc39"
+redirectUrl = "http://localhost:4444"
+
