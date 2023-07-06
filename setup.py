661121de77bef90fa3c455c0edcfba1ef363f016
Yoru <rickygogoi500@gmail.com>
2023-07-06 14:37:00 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import subprocess
 import os
-from config import mpdArtpath
 
 def isConfig(path):
     for file in os.listdir(path):
@@ -11,25 +10,25 @@ def isConfig(path):
 
 cwdpath = os.getcwd()
 track_path = os.path.join(cwdpath, "tracks")
+albumArtpath = os.path.join(cwdpath, 'art')
 
 if not isConfig(cwdpath):
     path = input("Enter the path of your music directory: ")
     cl_id = input("Enter your spotify client id: ")
     secret = input("Enter your spotify secret: ")
     url = input("Enter the redirect url of your spotify app: ")
+    mpdArtpath = input('Enter the path of your mpd client album art: ') 
 
     if not (
         os.path.exists(track_path) and not
         os.path.exists(os.path.join(cwdpath, 'art')) and
-        os.path.exists(mpdArtpath)
     ):
         os.mkdir(os.path.join(cwdpath, 'tracks'))
         os.mkdir(os.path.join(cwdpath, 'art'))
-        os.mkdir(mpdArtpath)
 
     with open("config.py", "w") as f:
         f.write(
-            f'Path = "{path}"\nclientId = "{cl_id}"\nTempPath = "{track_path}"\nclientSecret = "{secret}"\nredirectUrl = "{url}"')
+            f'Path = "{path}"\nclientId = "{cl_id}"\nTempPath = "{track_path}"\nclientSecret = "{secret}"\nredirectUrl = "{url}"\nmpdArtpath = "{mpdArtpath}"\nalbumArtpath = "{albumArtpath}"')
     print("config file generated!")
 
 print("Installing dependencies....")
